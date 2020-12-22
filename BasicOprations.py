2c1fc273931be32d2eaf82d00bdabc4bf400c079
navnathsatre <75266852+navnathsatre@users.noreply.github.com>
2020-12-22 22:27:46 +0530

    Add files via upload

---
+++ BasicOprations.py
@@ -0,0 +1,15 @@
+num1=int(input("Enter the value for num1 "))
+num2=int(input("Enter the value for num2 "))
+#num1=568
+#num2=64
+add=num1+num2
+multi=num1*num2
+sub=num1-num2
+div=num1/num2
+pow=num1**num2
+"""print("addition of num1 and num2 is:-",add)
+print("multiplication of num1 and num2 is:-",multi)
+print("substraction of num1 and num2 is:-",sub)
+print("power of num1 and num2 is:-",pow)
+print("division of num1 and num2 is:-%.5f"%(div))"""
+print(f"addition of two no. is {add}  multiplication of two no. is {multi} substraction of two no. is {sub} division is {div} power is{pow}")
